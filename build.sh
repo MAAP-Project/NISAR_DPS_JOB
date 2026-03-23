8eeb2ac9e11aabc54991a7bc08cc3b32e810eaa6
Harshini <harshinigirish2001@gmail.com>
2026-03-23 15:27:36 -0500

    Update build script to install additional packages
    Added package installations for earthaccess and others.

--- build.sh
+++ build.sh
@@ -11,3 +11,16 @@ cp -v "$SCRIPT_DIR/run.sh" /opt/app/run.sh
 
 chmod 755 /opt/app/nisar_access_subset.py
 chmod 755 /opt/app/run.sh
+
+mamba install -y -c conda-forge \
+  earthaccess \
+  h5py \
+  h5netcdf \
+  xarray \
+  zarr \
+  s3fs \
+  fsspec \
+  numpy \
+  pip
+
+pip install maap-py
